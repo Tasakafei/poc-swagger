5196e5a6b424dfdacba347de7700ba4d993fedcc
Alexandre Cazala <alexandre.cazala@gmail.com>
2022-10-06 08:49:12 +0200

    First commit

---
+++ src/main/java/com/capgemini/pocswagger/PocswaggerApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.pocswagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocswaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocswaggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capgemini/pocswagger/PocswaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.pocswagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocswaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
